8261905a1df270a15450d878c313478bde1ccd53
Melissa Hale <melissakayh@gmail.com>
2018-04-19 20:28:21 -0500

    adding work

--- PigLatin/Program.cs
+++ PigLatin/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace PigLatin
 {
@@ -6,16 +7,105 @@ namespace PigLatin
     {
         public static void Main()
         {
-            // your code goes here
+            
+            Console.WriteLine("Hello! Ready to have some fun with pig latin?");
+            Thread.Sleep(1500);
+            Console.WriteLine("Type any word or sentence, and I'll tell you the pig latin transalation!");
 
-            // leave this command at the end so your program does not close automatically
-            Console.ReadLine();
+            GetInput();
+        }
+
+        public static void GetInput()
+        {
+            string userInput;
+            string[] inputToArray;
+
+            Console.WriteLine("GO!");
+            userInput = Console.ReadLine();
+            Console.WriteLine("Great. Give me a second, and I'll get the translation for you.");
+            Thread.Sleep(1500);
+
+            inputToArray = userInput.Split(' ');
+            
+            TranslateSentence(inputToArray);
+        }
+
+        public static void TranslateSentence(string[] sentenceArray)
+        {
+            int arrayLength = sentenceArray.Length;
+            Console.WriteLine(arrayLength);
+
+            string[] translatedWords = new string[arrayLength];
+
+            //foreach (string word in sentenceArray)
+            for (int i = 0; i < arrayLength; i++)
+            {
+                string word = sentenceArray[i];
+                word = TranslateWord(word);
+                Console.WriteLine(word);
+                translatedWords[i] = word;
+            }
+            Console.WriteLine(translatedWords.Length);
+            Console.WriteLine(string.Join(" ", translatedWords)); //
+            PlayAgain();
+        }
+
+        public static string ScrubWord(string word)
+        {
+            word = word.ToLower();
+            return word;
         }
         
         public static string TranslateWord(string word)
         {
-            // your code goes here
-            return word;
+            word = ScrubWord(word);
+            string firstPart;
+            string restWord;
+            string translatedWord;
+            int firstVowelIndex;
+
+            firstVowelIndex = word.IndexOfAny(new char[] {'a', 'e', 'i', 'o', 'u'});
+
+            if (firstVowelIndex == 0 || firstVowelIndex == -1)
+            {
+                translatedWord = word + "yay";
+            }
+            else
+            {
+                firstPart = word.Substring(0,firstVowelIndex);
+                restWord = word.Substring(firstVowelIndex);
+                translatedWord = restWord + firstPart + "ay";
+            }
+            
+            return translatedWord;
+
+            // Results(translatedWord, word);
         }
+
+        public static void Results(string translatedWord, string word)
+        {
+            Console.WriteLine("The word {0} in piglatin is {1}", word, translatedWord);
+            Thread.Sleep(1500);
+
+            PlayAgain();
+        }
+
+        public static void PlayAgain()
+        {
+            string keepPlaying;   
+            Console.WriteLine("Another go? (y/n)");
+            keepPlaying = Console.ReadLine();
+
+            if (keepPlaying == "y")
+            {
+                GetInput();
+            }
+            else
+            {
+                Console.WriteLine("Thanks for stopping by!");
+            }
+
+        }
+
     }
 }
--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace RockPaperScissors
 {
@@ -8,18 +9,87 @@ namespace RockPaperScissors
         {
             Console.WriteLine("Enter hand 1:");
             string hand1 = Console.ReadLine().ToLower();
+            
+
             Console.WriteLine("Enter hand 2:");
             string hand2 = Console.ReadLine().ToLower();
             Console.WriteLine(CompareHands(hand1, hand2));
 
             // leave this command at the end so your program does not close automatically
-            Console.ReadLine();
+            Console.WriteLine("Play again? (y/n)");
+            string playAgain = Console.ReadLine();
+            PlayAgain(playAgain);
         }
         
         public static string CompareHands(string hand1, string hand2)
         {
-            // Your code here
-            return hand1 + ' ' + hand2;
+            if (hand1 == hand2)
+            {
+                return "tie";
+            }
+            else
+            {
+                if (hand1 == "rock")
+                {
+                    if (hand2 == "paper")
+                    {
+                        return hand1 + " loses to " + hand2;
+                    }
+                    else
+                    {
+                        return hand1 + " beats " + hand2;
+                    }
+                }
+                else if (hand1 == "paper")
+                {
+                    if (hand2 == "scissors")
+                    {
+                        return hand1 + " loses to " + hand2;
+                    }
+                    else
+                    {
+                        return hand1 + " beats " + hand2;
+                    }
+                }
+                else
+                {
+                    if (hand2 == "rock")
+                    {
+                        return hand1 + " loses to " + hand2;
+                    }
+                    else
+                    {
+                        return hand1 + " beats " + hand2;
+                    }
+                }
+            }
+            // return hand1 + ' ' + hand2;
         }
+
+        public static void PlayAgain(string playAgain)
+        {
+            if (playAgain == "y")
+            {
+                Main();
+            }
+            else
+            {
+                Console.WriteLine("Thanks for playing!");
+            }
+        }
+
+        public static bool ValidateEntry(string entry)
+        {
+            string[] validEntries = new string[] {"rock", "paper", "scissors"};
+            if (validEntries.Contains(entry))
+            {
+              return true;
+            }
+            else
+            {
+              return false;
+            }
+        }
+        
     }
 }
