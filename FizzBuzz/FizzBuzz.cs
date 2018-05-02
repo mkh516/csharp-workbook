1232593b29b4d991ed6de0b4499e3666c4224b80
Melissa Hale <melissakayh@gmail.com>
2018-05-01 19:49:20 -0500

    fizbuzz

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                num++;
+                if (num % 3 == 0 && num % 5 == 0)
+                {
+                    Console.WriteLine("Fizzbuzz", num);
+                }
+                else if (num % 3 == 0)
+                {
+                    Console.WriteLine("Fizz", num);
+                }
+                else if (num % 5 == 0)
+                {
+                    Console.WriteLine("Buzz", num);                
+                }
+                else
+                {
+                    Console.WriteLine(num);
+                }
+            }
+        }
+    }
+}
